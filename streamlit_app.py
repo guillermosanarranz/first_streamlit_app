1beba4023c5a64db96f14627c28d79d91d0e6324
Guillermo San Arranz <122017407+guillermosanarranz@users.noreply.github.com>
2023-01-05 04:33:45 -0600

    Update streamlit_app.py
    Added breakfast header and plates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.caption('Eggs & Bacon')
+streamlit.caption('Avocado Toast')
+streamlit.caption('Yogurt & Berries')
+
+
+
+
