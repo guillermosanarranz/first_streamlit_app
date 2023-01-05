d0ad9a39466f351ebf76308ce90ce82943fb16fc
Guillermo San Arranz <122017407+guillermosanarranz@users.noreply.github.com>
2023-01-05 04:44:38 -0600

    Update streamlit_app.py
    Added emojios to plate description

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Eggs & Bacon')
-streamlit.text('Avocado Toast')
-streamlit.text('Yogurt & Berries')
+streamlit.text('🐔 Eggs & Bacon')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥣 Yogurt & Berries')
 
 
 
