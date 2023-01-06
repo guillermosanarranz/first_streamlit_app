13e76b51a13085484296401190d582bc12f0ff18
Guillermo San Arranz <122017407+guillermosanarranz@users.noreply.github.com>
2023-01-06 05:05:05 -0600

    Update streamlit_app.py
    Corrected json capabilities

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,9 @@ streamlit.dataframe(fruits_to_show)
 #JSON
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
 
 
 
