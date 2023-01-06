8552a6b43e293b649c60e8e44176346f1b5069dd
Guillermo San Arranz <122017407+guillermosanarranz@users.noreply.github.com>
2023-01-06 15:47:15 -0600

    Update streamlit_app.py
    2nd try

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','')
   if not fruit_choice:
-    streamlit.write('The user entered ', fruit_choice)
+    streamlit.error("Please select a fruit to get information.")
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -59,7 +59,7 @@ streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_row)
 
 #Ask user for fruit input and use it to define json
-add_my_fruit = streamlit.text_input('What fruit would you like into add',' ')
+add_my_fruit = streamlit.text_input('What fruit would you like more information about',' ')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #Adding data to snowflake
