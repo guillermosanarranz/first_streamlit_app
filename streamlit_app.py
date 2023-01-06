e2b6ea038dae1d999ebd0d4ae6398f5f769ea6fc
Guillermo San Arranz <122017407+guillermosanarranz@users.noreply.github.com>
2023-01-06 15:38:07 -0600

    Update streamlit_app.py
    Does it still work

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents new healthy diner')
-
 streamlit.header('Breakfast Menu')
 
 streamlit.text('🐔 Eggs & Bacon')
@@ -20,14 +21,13 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 
-#JSON
-import requests
 
+
+#JSON
 #Ask user for fruit input and use it to define json
 fruit_choice = streamlit.text_input('What fruit would you like information about?','')
 streamlit.write('The user entered ', fruit_choice)
@@ -43,6 +43,11 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+
+
+#Don't run anything past here while we troubleshoot
+streamlit.stop()
+
 #Snowflake connector: 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -56,6 +61,9 @@ streamlit.dataframe(my_data_row)
 #Ask user for fruit input and use it to define json
 add_my_fruit = streamlit.text_input('What fruit would you like into add',' ')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#Adding data to snowflake
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
