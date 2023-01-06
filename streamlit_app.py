8d822d608fd9c009507e89e5a079f08c1a54a41c
Guillermo San Arranz <122017407+guillermosanarranz@users.noreply.github.com>
2023-01-06 05:09:44 -0600

    Update streamlit_app.py
    Substituted watermelon by kiwi on json call

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,9 @@ streamlit.dataframe(fruits_to_show)
 
 #JSON
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
 
 #Convert json to pandas df
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
