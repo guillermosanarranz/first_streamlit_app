a138ceebd6df5e01824805e83c730ca48c6c302e
Guillermo San Arranz <122017407+guillermosanarranz@users.noreply.github.com>
2023-01-06 15:44:58 -0600

    Update streamlit_app.py
    adding try-exception to user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,18 +29,18 @@ streamlit.dataframe(fruits_to_show)
 
 #JSON
 #Ask user for fruit input and use it to define json
-fruit_choice = streamlit.text_input('What fruit would you like information about?','')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
 streamlit.header("Fruityvice Fruit Advice!")
 
-#Convert json to pandas df
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#Show new pandas df
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','')
+  if not fruit_choice:
+    streamlit.write('The user entered ', fruit_choice)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 
 
