6f6e4144c7020107bf5bd086fadd59b6344a45b6
Guillermo San Arranz <122017407+guillermosanarranz@users.noreply.github.com>
2023-01-06 05:11:08 -0600

    Update streamlit_app.py
    Added user input capabilities to select fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,12 @@ streamlit.dataframe(fruits_to_show)
 
 #JSON
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+#Ask user for fruit input and use it to define json
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
