9b806e313d5f26deb31ee3162a7af2fc35158796
Guillermo San Arranz <122017407+guillermosanarranz@users.noreply.github.com>
2023-01-07 03:46:49 -0600

    Update streamlit_app.py
    Wrapped in a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import streamlit
 import pandas
 import requests
 import snowflake.connector
@@ -27,18 +28,22 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-#JSON
-#Ask user for fruit input and use it to define json
-streamlit.header("Fruityvice Fruit Advice!")
+#Create the repeatable code block alled function
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
+
+#New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
@@ -64,8 +69,3 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 #Adding data to snowflake
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-
-
-
-
